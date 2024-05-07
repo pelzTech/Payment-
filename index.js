8557ea141a47e5e7da5a9e5c22197a1f0de68b47
pelzTech <165785121+pelzTech@users.noreply.github.com>
2024-05-07 04:43:13 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+ // Gather form data
+ const formData = {
+    firstName: document.getElementById('firstName').value,
+    middleName: document.getElementById('middleName').value,
+    lastName: document.getElementById('lastName').value,
+    username: document.getElementById('username').value,
+    phoneNumber: document.getElementById('phoneNumber').value,
+    age: document.getElementById('age').value,
+    nationality: document.getElementById('nationality').value,
+    maritalStatus: document.getElementById('maritalStatus').value,
+    address: document.getElementById('address').value,
+    paymentOption: document.getElementById('paymentOption').value
+  };
+
+
